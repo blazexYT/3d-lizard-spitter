5081434f1b5ae81bea8200d564f9d9cdbccb58cd
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-11 10:39:20 +1200

    First commit

---
+++ dino vs lizzards/game models/menu.gd
@@ -0,0 +1,26 @@
+extends Control
+@onready var MOUSE_MODE_VISIBLE = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$"VBoxContainer/levels button".grab_focus()
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+
+	pass
+		
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://dino vs lizzards/game models/main'.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func _on_levels_button_pressed():
+	get_tree().change_scene_to_file("res://levelsmenu.tscn")
---
+++ dino vs lizzards/game models/pausemenu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+@onready var MOUSE_MODE_VISIBLE = 0
+#@onready var pause_menu = $pausemenu
+var paused = false 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+		pass
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_resume_pressed():
+	get_tree().change_scene_to_file("res://dino vs lizzards/game models/main'.tscn")
+	
+
+
+func _on_menu_pressed():
+	get_tree().change_scene_to_file("res://dino vs lizzards/game models/menu.tscn")
+
+
