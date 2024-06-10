4161bd22799e14f16d8cc5850c381f1c93682ede
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-11 11:00:30 +1200

    error fix

--- dino vs lizzards/game models/main'.gd
+++ dino vs lizzards/game models/main'.gd
@@ -6,10 +6,10 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
-@onready var pause_menu = $pausemenu
+#@onready var pause_menu = $pausemenu
 var paused = false 
 
 
@@ -17,4 +17,5 @@ var paused = false
 func _process(_delta):
 	if Input.is_action_just_pressed("pause"):
 		get_tree().change_scene_to_file("res://dino vs lizzards/game models/pausemenu.tscn")
-	
+	if Input.is_action_just_pressed("respawn"):
+		get_tree().reload_current_scene()
